28c5f7429563ddb8cf0f8eef2ec8472c06ca950f
Ali Parvizi <snparvizi75@gmail.com>
2020-04-25 00:38:15 +0430

    One active game per user | proper loggin info

--- index.js
+++ index.js
@@ -16,137 +16,170 @@ function reportError(ws, msg){
   }))
 }
 
+
 wss.on('connection', (ws) => {
-  try {
-  ws.on('message', (msg) => {
+    console.log("[Server] Accepting new connection");
+
     try {
-      // get game data in json
-      let game_data = JSON.parse(msg);
+    ws.on('message', (msg) => {
+        console.log("[Server] Processing new message");
+        console.log("[Server] active games " + games.length);
+        try {
+        // get game data in json
+        let game_data = JSON.parse(msg);
 
-      // a post type is used to create a new game
-      if (game_data.type == "POST"){
+        // a post type is used to create a new game
+        if (game_data.type == "POST"){
+            // only one active game at the same time
+            var gameObjs = games.filter((item) => {
+                return item.hasPlayer(ws);
+            });
 
-        // create a game object
-        var gameObj = new game.Game(game_data.size, game_data.winBy, game_data.starter);
-        
-        // add the current ws socket as one of the players
-        gameObj.addPlayer(ws);
+            gameObjs.forEach(element => {
+                games.splice(games.indexOf(element),1);
+            });
 
-        // save the newly created game object
-        games.push(gameObj);
+            console.log("[Server] Creating new game");
 
-        // notify the creator with the auto generated gameId
-        ws.send(JSON.stringify({
-          'status': 201,
-          'gameId': gameObj.gameId
-        }))
+            // create a game object
+            var gameObj = new game.Game(game_data.size, game_data.winBy, game_data.starter);
+            
+            // add the current ws socket as one of the players
+            gameObj.addPlayer(ws);
 
-      } else if (game_data.type == "PUT") {
-        // boardcast a move played in a game to all player exclude self
-        let move = game_data.move; // (i, j)
-        let gameId = game_data.gameId;
+            // save the newly created game object
+            games.push(gameObj);
 
-        var gameObj = games.find((item)=>{
-          return item.gameId == gameId;
-        });
-
-        if (gameObj == null){
-          ws.send(JSON.stringify({
-              'status': 404,
-              'details': "Invalid game id"
-          }));
-          return;
-        }
-
-        var players = gameObj.players.filter((item) => {
-          return !(item == ws);
-        })
-
-        for (var i = 0; i < players.length; i++){
-          players[i].send(JSON.stringify({
-            'status': 300,
-            'gameId': gameId,
-            'move': move
-          }));
-        }
-
-      } 
-      // to join a game that has allready been created
-      else if (game_data.type == "JOIN"){
-        // get the game id
-        let gameId = game_data.gameId;
-        let gameObj = null;
-        
-        // search for a with the given gameId
-        gameObj = games.find((item)=>{
-          return item.gameId == gameId;
-        });
-
-        // if gameObj is null, then the given gameId is invalid
-        if (gameObj == null){
-          ws.send(JSON.stringify({
-              'status': 404,
-              'details': "Invalid game id"
-          }));
-          return;
-        }
-        
-        // add the requesting web socket as one of the players
-        gameObj.addPlayer(ws);
-
-        // notify with game information
-        ws.send(JSON.stringify({
-          'status': 200,
-          'game': {
-            'size': gameObj.size,
-            'winBy': gameObj.winBy,
-            'starter': gameObj.starter,
+            // notify the creator with the auto generated gameId
+            ws.send(JSON.stringify({
+            'status': 201,
             'gameId': gameObj.gameId
-          }
-        }))
-      
-      // and invalid request type has been submitted
-      } else {
-        ws.send(JSON.stringify({
-          'status': 400,
-          'details': "Bad request"
-        }));
-      }
+            }))
+            console.log("[Server] New game created with id " + gameObj.gameId);
+
+        } else if (game_data.type == "PUT") {
+            console.log("[Server] Making new move");
+
+            // boardcast a move played in a game to all player exclude self
+            let move = game_data.move; // (i, j)
+            let gameId = game_data.gameId;
+
+            var gameObj = games.find((item)=>{
+            return item.gameId == gameId;
+            });
+
+            if (gameObj == null){
+            ws.send(JSON.stringify({
+                'status': 404,
+                'details': "Invalid game id"
+            }));
+            return;
+            }
+
+            var players = gameObj.players.filter((item) => {
+            return !(item == ws);
+            })
+
+            for (var i = 0; i < players.length; i++){
+            players[i].send(JSON.stringify({
+                'status': 300,
+                'gameId': gameId,
+                'move': move
+            }));
+            }
+
+            console.log("[Server] Move broadcasted " + move);
+
+        } 
+        // to join a game that has allready been created
+        else if (game_data.type == "JOIN"){
+            console.log("[Server] Joining to game "  + game_data.gameId );
+
+            // get the game id
+            let gameId = game_data.gameId;
+            let gameObj = null;
+            
+            // search for a with the given gameId
+            gameObj = games.find((item)=>{
+            return item.gameId == gameId;
+            });
+
+            // if gameObj is null, then the given gameId is invalid
+            if (gameObj == null){
+            ws.send(JSON.stringify({
+                'status': 404,
+                'details': "Invalid game id"
+            }));
+            return;
+            }
+            
+            // add the requesting web socket as one of the players
+            gameObj.addPlayer(ws);
+
+            // notify with game information
+            ws.send(JSON.stringify({
+            'status': 200,
+            'game': {
+                'size': gameObj.size,
+                'winBy': gameObj.winBy,
+                'starter': gameObj.starter,
+                'gameId': gameObj.gameId
+            }
+            }))
+
+            console.log("[Server] Joined to game "  + game_data.gameId );
+        // and invalid request type has been submitted
+        } else {
+            console.log("[Server | User Error] Invalid request type "  + game_data.type );
+
+            ws.send(JSON.stringify({
+            'status': 400,
+            'details': "Bad request"
+            }));
+        }
+        } catch (err) {
+        console.log("[Server | Error] " + err.message );
+        console.log("[Server | Error] " + err.stack);
+        reportError(ws, err.message);
+        }
+    });
+
+    ws.on('close', () => {
+        console.log("[Server] Closing connection ");
+
+        try {
+        // remove the ws from game players
+        var gameObjs = games.filter((item) => {
+            return item.hasPlayer(ws);
+        });
+
+        for (var i  = 0; i < gameObjs.length;i++){
+            var gameObj = gameObjs[i];
+
+            gameObj.removePlayer(ws);
+
+            // if the game has no players left, delete it
+            if (gameObj.players.length == 0){
+                games.splice(games.indexOf(gameObj, 1))
+                console.log("[Server] Game deleted " + gameObj.gameId);
+            }
+            console.log("[Server] Connection closed");
+        }
+        
     } catch (err) {
-      console.log("[Server | Error] " + err.message );
-      console.log("[Server | Error] " + err.stack);
-      reportError(ws, err.message);
-    }
-  });
+        console.log("[Server | Error] " + err.message );
+        console.log("[Server | Error] " + err.stack);
+        reportError(ws, err.message);
 
-  ws.on('close', () => {
-    try {
-      // remove the ws from game players
-      var gameObj = games.find((item) => {
-        return item.hasPlayer(ws);
-      });
+        }
 
-      if (gameObj == null)
-        return
-
-      gameObj.removePlayer(ws);
-
-      // if the game has no players left, delete it
-      if (gameObj.players.length == 0)
-        games.splice(games.indexOf(gameObj, 1))
-  
+    });
     } catch (err) {
-      console.log("[Server | Error] " + err.message );
-      console.log("[Server | Error] " + err.stack);
-      reportError(ws, err.message);
-
+        console.log("[Server | Error] " + err.message );
+        console.log("[Server | Error] " + err.stack);  
+        reportError(ws, err.message);
     }
 
-  });
-  } catch (err) {
-    console.log("[Server | Error] " + err.message );
-    console.log("[Server | Error] " + err.stack);  
-    reportError(ws, err.message);
-  }
+    console.log("[Server] Connection Established ");
 
 });
